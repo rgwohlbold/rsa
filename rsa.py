075e143c4fb719c14faa0f67f4f2501574dff919
Richard Wohlbold <richard.wohlbold@gmail.com>
2018-01-10 16:50:19 -0600

    Finished project

--- rsa.py
+++ rsa.py
@@ -2,11 +2,13 @@ import sys
 import math
 import random
 
+# Class implementing the RSA cryptosystem
+# Upon initialization, this randomly generates a RSA key pair
+# It provides encrypt(text) and decrypt(num) as its methods designed for external use
 class RSA:
     
-    #alphabet = " ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜabcdefghijklmnopqrstuvwxyzäöü1234567890!\"§$%&/()[]={}?\\`´*+~#'-_.:,;<>"
-
-    alphabet=" abcdefghijklmnopqrstuvwxyz"
+    alphabet = " ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜabcdefghijklmnopqrstuvwxyzäöü1234567890!\"§$%&/()[]={}?\\`´*+~#'-_.:,;<>"
+    #alphabet="%abcdefghijklmnopqrstuvwxyz "
 
     # Initialize all instance variables
     def __init__(self, length=5):
@@ -16,31 +18,15 @@ class RSA:
         print("Calculating n...")
         self.n = self.p * self.q
         self.phi = (self.p - 1) * (self.q - 1)
-        self.e = self.choose_e(self.phi)
+        self.d = self.choose_d(self.phi)
         print("Finding inverse...")
-        self.d = self.modular_inverse(self.e, self.phi)
-        #self.d = pow(self.e, self.phi - 1, self.phi)
-        print("Done!")
-
-    # Very simple and inefficient primality test
-    def is_prime(self,number):
-        for i in range(2, math.ceil(math.sqrt(number)) + 1):
-            if number % i == 0:
-                return False
-        return True
-   
-    def modular_inverse(self, a, b):
-        start_b = b
-        print("modular inverse", a,b)
-        alpha,s,beta,t = 1,0,0,1
-        while b != 0:
-            q = a // b
-            a, b = b, a % b
-            alpha, s = s, alpha - q * s
-            #beta, t = t, beta - q * t
-        #return (a, alpha, beta)
-        print(a, alpha, b)
-        return alpha % start_b
+        self.e = self.modular_inverse(self.d, self.phi)
+        # The number of characters that fit into a block (only if the alphabet size is <=100)
+        self.chars_per_block = math.floor(math.log(self.n, 10)) // 2
+        # The number of digits that can be encoded. This is one less than the number of digits of n since if it was higher, the program could not guarantee that no block exceeds n
+        # The number of digits in an encrypted block is higher since these can have the amount of digits n has, but in a controlled way since the encryption always goes mod n
+        self.digits_per_block = math.floor(math.log(self.n, 10))
+    
 
     # Generates random prime number of the desired length inefficiently
     def random_prime(self, length=5):
@@ -48,97 +34,136 @@ class RSA:
         while not self.is_prime(i):
             i = random.randint(10**(length-1), 10**length)
         return i
+
+    # Very simple and inefficient primality test
+    def is_prime(self,number):
+        for i in range(2, math.ceil(math.sqrt(number)) + 1):
+            if number % i == 0:
+                return False
+        return True
+
+    # Calculate modular inverse with Extended Euclidian Algorithm
+    def modular_inverse(self, a, b):
+        start_b = b
+        alpha,s,beta,t = 1,0,0,1
+        while b != 0:
+            q = a // b
+            a, b = b, a % b
+            alpha, s = s, alpha - q * s
+            #beta, t = t, beta - q * t
+        #return (a, alpha, beta)
+        return alpha % start_b
    
-    # Chooses either 65537 or 17 for e with given phi
-    def choose_e(self, phi):
+    # Chooses either 65537 or 17 for d with given phi
+    def choose_d(self, phi):
         if phi > 65537 and math.gcd(phi, 65537) == 1:
             return 65537
         elif phi > 17 and math.gcd(phi, 17) == 1:
             return 17
         else:
             raise Error("Invalid phi for this implementation")
+   
+
+    # The following encryption and decryption functions are in order of application
 
 
-    # Convert a list of numbers to the corresponding string with the given alphabet
-    def to_string(self, nums):
-        length = math.floor(math.log(self.n, 10))    # Find length of public N
-        nums_per_block = length // 2                # Every character takes up two digits
-        string = ""
-        for i in nums:
-
-            temp = ""
-            next = i
-            for j in range(nums_per_block):
-                num = next % 100
-                temp += self.alphabet[num]
-                next = (next - num) // 100
-            string += temp[::-1]
-        return string
-
-    
-    # Converts a string to a list of numbers with a block length smaller than N
-    # Note: Alphabet length currently hardcoded to less <= 10**2
-    def to_nums(self, string):
-        length = math.floor(math.log(self.n, 10))    # Find length of public N
-        nums_per_block = length // 2                # Every character takes up two digits
+    # Splits a string into pieces of length chars_per_block
+    def split_string(self, string):
         blocks = []
-        j = 0
-        currentBlock = 0
-        for i in string:
-            # Shift block to the left and add new number
-            try:
-                currentBlock = currentBlock * 100 + self.alphabet.index(i)
-            except ValueError:
-                print("Error: The letter '" + i + "' was not found in the alphabet. Please delete it and try again.")
-                sys.exit(-1)
-            j += 1
-            # Finished block creation, appending it to the list
-            if j == nums_per_block:
-                blocks.append(currentBlock)
-                j = 0
-                currentBlock = 0
-        while j < nums_per_block and j != 0:
-            currentBlock = currentBlock * 100 + 0
-            j += 1
-            if j == nums_per_block:
-                blocks.append(currentBlock)
+        for i in range(0, len(string), self.chars_per_block):
+            blocks.append(string[i:i+self.chars_per_block])
         return blocks
 
-    # Decrypts single number
-    def decrypt_num(self, num):
-        print(num, self.d, self.n, pow(num, self.d, self.n))
-        return pow(num, self.d, self.n) # Modular exponentiation
+    # Converts a string into a number
+    def string_to_number(self, string):
+        result = 0
+        for i in string:
+            result = result * 100 + self.alphabet.index(i)
+        return result
     
-    # Encrypts single number
-    def encrypt_num(self, num):
-        print(num, self.e, self.n, pow(num, self.e, self.n))
-        return pow(num, self.e, self.n) # Modular exponentiation
-
-    # Encrypt a string
-    def encrypt(self, text):
-        nums = self.to_nums(text)
-
+    # Encrypts a list of blocks and returns a list of encrypted blocks
+    def encrypt_blocks(self, blocks):
         encrypted = []
-        for i in nums:
-            encrypted.append(self.encrypt_num(i))
+        for block in blocks:
+            encrypted.append(pow(block, self.d, self.n))
         return encrypted
+    
+    # Concatenates a list of blocks into a single number
+    def concat_blocks(self, blocks):
+        number = 0
+        for block in blocks:
+            number = number * (10 ** (self.digits_per_block + 1)) + block
+        return number
 
-    # Decrypt a list of numbers
-    def decrypt(self, nums):
+    # Splits a number into blocks of length self.digits_per_block + 1
+    def split_number(self, number):
+        factor = 10 ** (self.digits_per_block + 1)
+        blocks = []
+        while number != 0:
+            blocks.append(number % factor)
+            number = number // factor
+        return blocks[::-1]
+
+    # Decrypt a list of blocks
+    def decrypt_blocks(self, blocks):
         decrypted = []
-        for i in nums:
-            decrypted.append(self.decrypt_num(i))
-        return self.to_string(decrypted)
+        for block in blocks:
+           decrypted.append(pow(block, self.e, self.n)) 
+        return decrypted
+
+    # Convert a number to a string given the defined alphabet
+    def number_to_string(self, number):
+        string = ""
+        while number != 0:
+            string += self.alphabet[number % 100]
+            number = number // 100
+        return string[::-1]
+
+    # Concat a list of strings
+    def concat_string(self, strings):
+        result = ""
+        for string in strings:
+            result += string
+        return result
+        
+    # Method to encrypt a piece of text
+    def encrypt(self, text):
+        split = self.split_string(text)
+        blocks = []
+        for i in split:
+            blocks.append(self.string_to_number(i))
+        encrypted = self.encrypt_blocks(blocks)
+        return self.concat_blocks(encrypted)
+    
+    # Encrypt to string. Only works reliably with full alphabet
+    def encrypt_to_string(self, text):
+        assert(len(self.alphabet) == 100)
+        num = self.encrypt(text)
+        return self.number_to_string(num)
+    
+    # Decrypt from string. Only works reliably with full alphabet
+    def decrypt_from_string(self, text):
+        assert(len(self.alphabet) == 100)
+        num = self.string_to_number(text)
+        return self.decrypt(num)
+
+    # Method to decrypt a piece of text
+    def decrypt(self, number):
+        blocks = self.split_number(number)
+        decrypted = self.decrypt_blocks(blocks)
+        strings = []
+        for i in decrypted:
+            strings.append(self.number_to_string(i))
+        return self.concat_string(strings)
 
     # Returns string representation of instance
     def __str__(self):
-        result = ""
-        result += "p\t" + str(self.p) + "\n"
+        result = "p\t" + str(self.p) + "\n"
         result += "q\t" + str(self.q) + "\n"
         result += "n\t" + str(self.n) + "\n"
         result += "phi\t" + str(self.phi) + "\n"
-        result += "d(priv)\t" + str(self.e) + "\n"
-        result += "e(pub)\t" + str(self.d) + "\n"
+        result += "d(pub)\t" + str(self.d) + "\n"
+        result += "e(priv)\t" + str(self.e) + "\n"
         return result
 
 # Main RSA test function
@@ -146,11 +171,11 @@ def main(text, length=5):
     rsa = RSA(length)
 
     # Encrypt number
-    enc = rsa.encrypt(text)
-    print("Encrypted:", enc)
-    dec = rsa.decrypt(enc)
+    enc = rsa.encrypt_to_string(text)
+    print(enc)
+    #print(rsa)
+    dec = rsa.decrypt_from_string(enc)
     print(dec)
-    print(rsa)
 
 # If rsa.py is executed directly, call main()
 if __name__ == "__main__":
